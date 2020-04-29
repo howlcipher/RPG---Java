0dd4c20d0a93963edea2a644920d64ec750156b2
howlcipher <wylelias.123@gmail.com>
2020-04-29 19:03:51 -0400

    started on menu creation

--- Main.java
+++ Main.java
@@ -3,9 +3,17 @@ import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
+    String job = "Warrior";
+    switch (job.toUpperCase()){
+      case "WARRIOR":
+        character Thor = new warrior("Thor", job);
+        Thor.display();
+      default:
+        System.out.println("No character created.");
+        break;
+    }
 
-    character Thor = new warrior("Thor", "Warrior");
-    Thor.display();
+
 
   }
 }
